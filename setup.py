80321940322390ee38d162572fc6d39ad73b0170
aarnisi <140318765+aarnisi@users.noreply.github.com>
2023-10-25 22:26:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='dbclusters',
     version='1.0',
-    url = 'https://github.com/aarnisi/databricks-cluster-optimization',
+    url = 'https://github.com/aarnisi/databricks_cluster_optimization',
     packages=find_packages(),
     install_requires=[
         'databricks'
